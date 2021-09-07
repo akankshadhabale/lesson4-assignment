713ffd4077bcbde22b3c8f50076f032a07567257
akanksha dhabale <akankshadhabale9158@gmail.com>
2021-09-07 19:33:27 +0530

    added js

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-var inputText=document.querySelector("#txt-input");
+var btnTranslate=document.querySelector("#btn-translate");
+var txtInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
 
-console.log(inputText.value);
+var serverURL="https://api.funtranslations.com/translate/ferb-latin.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+
+}
+
+function errorHandler(error){
+    console.log("error occured", error)
+}
+
+function clickedHandler(){
+
+var inputText = txtInput.value;
+
+
+  fetch(getTranslationURL(inputText))
+  .then(response => response.json())
+  .then(json => {
+      var translatedText=json.contents.translated;
+    outputDiv.innerText=translatedText;
+    })
+.catch(errorHandler) 
+}
+
+btnTranslate.addEventListener("click",clickedHandler)
+
